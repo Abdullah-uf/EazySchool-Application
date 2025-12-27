cd9e94b76ff53b0b5043131af509441398e39bdb
Abdullah uf <Abdullahumar1059@gmail.com>
2025-12-27 22:39:22 +0530

    Updated feature

--- src/main/java/com/eazybytes/eazyschool/service/PersonService.java
+++ src/main/java/com/eazybytes/eazyschool/service/PersonService.java
@@ -1,6 +1,32 @@
 package com.eazybytes.eazyschool.service;
 
-import com.eazybytes.eazyschool.model.Person;
-import jakarta.validation.Valid;
 
-public class PersonService {}
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Roles;
+import com.eazybytes.eazyschool.repository.PersonRepository;
+import com.eazybytes.eazyschool.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+    public boolean createNewPerson(Person person){
+        boolean isSaved = false;
+        Roles role = rolesRepository.getByRoleName(com.eazybytes.eazyschool.constants.EazySchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+        person = personRepository.save(person);
+        if (null != person && person.getPersonId() > 0)
+        {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+
+}
