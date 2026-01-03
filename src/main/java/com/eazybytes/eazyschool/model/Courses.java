cef19defe2c07ff974bde270697212c9d5dcd8ca
Abdullah uf <Abdullahumar1059@gmail.com>
2026-01-03 15:06:33 +0530

    Updated feature

--- src/main/java/com/eazybytes/eazyschool/model/Courses.java
+++ src/main/java/com/eazybytes/eazyschool/model/Courses.java
@@ -1,4 +1,26 @@
 package com.eazybytes.eazyschool.model;
 
-public class Courses {
+import lombok.Getter;
+import lombok.Setter;
+
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Courses extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int courseId;
+
+    private String name;
+
+    private String fees;
+
+    @ManyToMany(mappedBy = "courses", fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
+    private Set<Person> persons = new HashSet<>();
 }
--- src/main/java/com/eazybytes/eazyschool/repository/CoursesRepository.java
+++ src/main/java/com/eazybytes/eazyschool/repository/CoursesRepository.java
@@ -1,4 +1,10 @@
 package com.eazybytes.eazyschool.repository;
 
-public interface CoursesRepository {
+import com.eazybytes.eazyschool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
 }
