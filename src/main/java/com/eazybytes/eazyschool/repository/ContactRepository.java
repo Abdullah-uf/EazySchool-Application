01d12719127effde85215ddff997b1c1222e6a37
Abdullah uf <Abdullahumar1059@gmail.com>
2026-01-05 19:24:38 +0530

    Updated feature

--- src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
+++ src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -3,11 +3,15 @@ package com.eazybytes.eazyschool.repository;
 
 import com.eazybytes.eazyschool.model.Contact;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,6 +22,27 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
     List<Contact> findByStatus(String status);
 
-    Page<Contact> findByStatus(String status, Pageable pageable);
+    @Query("SELECT c FROM Contact c WHERE c.status = :status")
+
+    Page<Contact> findByStatus(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+    int updateStatusById(String status, int id);
+
+    Page<Contact> findOpenMsgs(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    int updateMsgStatus(String status, int id);
+
+    @Query(nativeQuery = true)
+    Page<Contact> findOpenMsgsNative(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true)
+    int updateMsgStatusNative(String status, int id);
 
 }
