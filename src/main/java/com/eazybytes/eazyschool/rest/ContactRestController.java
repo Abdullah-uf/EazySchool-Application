e3d1ac584b9871412fb79f4188c2b6f90c12ea14
Abdullah uf <Abdullahumar1059@gmail.com>
2026-01-08 00:27:16 +0530

    Updated feature

--- src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
+++ src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -64,6 +65,26 @@ public class ContactRestController {
         Response response = new Response();
         response.setStatusCode("200");
         response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact contactReq){
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(contactReq.getContactId());
+        if(contact.isPresent()){
+            contact.get().setStatus(com.eazybytes.eazyschool.constants.EazySchoolConstants.CLOSE);
+            contactRepository.save(contact.get());
+        }else{
+            response.setStatusCode("400");
+            response.setStatusMsg("Invalid Contact ID received");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully closed");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
